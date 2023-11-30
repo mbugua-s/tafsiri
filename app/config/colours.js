f6e013a661007b3c942dcdb514199774b7329a8c
mbugua-s <smbuguaaa@gmail.com>
2023-11-30 20:21:05 +0300

    Uploaded files, added basic login screen

---
+++ app/config/colours.js
@@ -0,0 +1,7 @@
+export default {
+    black: "#000",
+    primary: "dodgerblue",
+    secondary: "blue",
+    white: "white",
+    textInputBackgroundColor: "#dedddc"
+}
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,112 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+import colours from '../config/colours'
+import { TextInput } from 'react-native'
+import { Button } from 'react-native'
+import { Pressable } from 'react-native'
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <View style = {styles.container}>
+            <Text style = {styles.title}>Tafsiri</Text>
+            <Text style = {styles.description}>Log in to continue</Text>
+
+            <View style = {styles.inputView}>
+                <TextInput
+                    placeholder='Email'
+
+                    placeholderTextColor={colours.black}
+                    style = {styles.inputText}
+                ></TextInput>
+            </View>
+
+            <View style = {styles.inputView}>
+                <TextInput
+                    placeholder='Password'
+                    secureTextEntry
+                    placeholderTextColor={colours.black}
+                    style = {styles.inputText}
+                ></TextInput>
+            </View>
+
+            <View style = {styles.buttonArea}>
+                <Pressable
+                    style={styles.button}>
+                    <Text style = {styles.buttonText}>Log In</Text>
+                </Pressable>
+                
+                <Pressable
+                    style={[styles.button, styles.registerButton]}>
+                    <Text style = {styles.buttonText}>Register</Text>
+                </Pressable>
+            </View>
+            
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: 'center',
+        alignItems: "center",
+    },
+    
+    title: {
+        color: colours.primary,
+        fontSize: 50,
+        fontWeight: '700',
+        bottom: "7%"
+    },
+    
+    description: {
+        fontSize: 17,
+        bottom: '5%',
+        
+    },
+
+    inputView: {
+        width: "80%",
+        backgroundColor: colours.textInputBackgroundColor,
+        borderRadius: 15,
+        height: 50,
+        marginBottom: 20,
+        justifyContent: "center",
+        padding: 20
+    },
+
+    inputText: {
+        height: 50,
+        color: colours.black
+    },
+
+    buttonArea: {
+        backgroundColor: 'gold',
+        alignItems: 'center',
+        // flex: 1,
+        width: '100%'
+        // top: '10%'
+    },
+
+    button: {
+        backgroundColor: colours.primary,
+        width: "50%",
+        height: 40,
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+    },
+
+    buttonText: {
+        color: colours.white,
+        
+    },
+
+    registerButton: {
+        backgroundColor: colours.secondary
+    }
+})
