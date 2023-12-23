073e944f6d70edbe0967db5227ee464d46f9f52a
mbugua-s <smbuguaaa@gmail.com>
2023-12-23 23:21:47 +0300

    [React Navigation Tutorial] Learnt how to pass params

--- App.js
+++ App.js
@@ -1,61 +1,58 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import LoginScreen from './app/screens/LoginScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen({ navigation })
+
+function HomeScreen({ navigation, route })
 {
+    React.useEffect(() => {
+        if (route.params?.post) {
+            // Post updated, do something with `route.params.post`
+            // For example, send the post to the server
+        }
+    }, [route.params?.post])
+    
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
             <Button 
-                title="Go to Details"
+                title="Create post"
                 onPress={() => 
-                    navigation.navigate('Details', {
-                        // Navigate to the Details route with params
-                        itemId: 86,
-                        otherParam: 'anything you want here'
-                    })}
+                    navigation.navigate('CreatePost')}
             />
+            <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
         </View>
     );
 }
 
-function DetailsScreen({ route, navigation }) {
+function CreatePostScreen({ route, navigation }) {
     // Get the params
 
-    const { itemId, otherParam } = route.params
+    const [postText, setPostText] = React.useState('')
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Details Screen</Text>
-            <Text>itemId: {JSON.stringify(itemId)}</Text>
-            <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-            <Button 
-                title="Go to Details...again"
-                onPress={() => 
-                    navigation.push('Details', {
-                        itemId: Math.floor(Math.random() * 100)
-                    })}
+        <>
+            <TextInput
+                multiline
+                placeholder="Whats on your mind?"
+                style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+                value={postText}
+                onChangeText={setPostText}
             />
-            <Button 
-                title="Go to Home"
-                onPress={() => 
-                    navigation.navigate('Home')}
-            />
-            <Button 
-                title="Go back"
-                onPress={() => 
-                    navigation.goBack()}
-            />
-            <Button 
-                title="Go back to first screen in stack"
-                onPress={() => 
-                    navigation.popToTop()}
-            />
-        </View>
+            <Button
+                title="Done"
+                onPress={() => {
+                    navigation.navigate({
+                        name: 'Home',
+                        params: { post: postText },
+                        merge: true
+                    })
+                }}
+            ></Button>
+        </>
     )
 }
 
@@ -65,8 +62,8 @@ export default function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator initialRouteName='Home'>
-            <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'Overview'}} />
-            <Stack.Screen name='Details' component={DetailsScreen}/>
+            <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'My Home Screen'}} />
+            <Stack.Screen name='CreatePost' component={CreatePostScreen} initialParams={{ itemId: 42 }} />
         </Stack.Navigator>
     </NavigationContainer>
   );
